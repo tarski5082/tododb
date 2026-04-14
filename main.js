9ee6271e480f1d5fff82adcdd2e0b0610f69916e
tarski alanne <zecht.276@gmail.com>
2026-04-14 13:22:31 +0200

    electron

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-import prisma from "./db.js";
+const prisma =require('./db.js');
+const {app, BrowserWindow} = require('electron');
+const path = require('path')
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences:{
+            preload:path.join(__dirname,'preload.js'),
+            contextIsolation:true,
+            nodeIntegration:false
+        }
+    });
+    win.loadFile('renderer/index.html');
+}
+
+app.whenReady().then(createWindow);
 
 
 
@@ -36,7 +53,4 @@ async function updateDoneTask(id_){
         },
     })
 }
-updateDoneTask(2);
-
-
 
